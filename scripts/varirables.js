757d3d823b52063becc5c151bab8fdba06c7f91c
will <william.layne.richardson@gmail.com>
2024-08-21 16:16:15 -0600

    variables

---
+++ scripts/varirables.js
@@ -0,0 +1,10 @@
+
+// this variable is a number
+let intelligenceQuoteint = 100;
+
+let firstName = "sawyer";
+let lastName = "richardson";
+
+firstName = "will"
+
+console.log(firstName,lastName,"has IQ",intelligenceQuoteint)
