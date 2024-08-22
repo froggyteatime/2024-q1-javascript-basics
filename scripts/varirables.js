f206bfff356575258ef7203c3ced82fc7b8f2f9d
will <william.layne.richardson@gmail.com>
2024-08-22 16:23:59 -0600

    learning varirabes

--- scripts/varirables.js
+++ scripts/varirables.js
@@ -1,10 +1,46 @@
-
+//@ts-check
 // this variable is a number
 let intelligenceQuoteint = 100;
 
+// first name is a string
 let firstName = "sawyer";
 let lastName = "richardson";
 
+//I can reassign a variable's value
 firstName = "will"
+//const can not be changed
+const MIN_IQ = 50;
+const MAX_IQ = 300;
 
-console.log(firstName,lastName,"has IQ",intelligenceQuoteint)
+//MIN_IQ = 100;
+
+// Boolean values are true or false
+let isWillTheCoolest = true;
+
+// undefined
+let dontKnow;
+
+// null is a value that is no value
+let intoTheVoid = null;
+
+console.log(firstName,lastName,"has IQ",intelligenceQuoteint);
+
+
+let myPet = {
+      type:"Dog",
+      name:"winnie",
+      ageInHumanYears: 5,
+      color: "tri"
+};
+myPet.color = "tri color";
+console.log ("myPet looks like this",myPet);
+
+// arrays are fun
+let favCandies = [
+    "war heads",
+    "black licorice",
+    "skittles",
+];
+console.log("my fav candies are",favCandies);
+//arrays start counting from zero
+console.log("my fav candies at index 2 is",favCandies);
