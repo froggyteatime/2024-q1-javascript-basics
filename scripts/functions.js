5109847c60978e55c000000538395578a650ea53
will <william.layne.richardson@gmail.com>
2024-08-26 16:10:32 -0600

    i did it

---
+++ scripts/functions.js
@@ -0,0 +1,12 @@
+//@ts-check
+
+let sayHi = function(){
+    let firstName = "Will";
+    let age = 14;
+    let greeting = "Hello My Name is"+firstName+"and I am"+age+"years old";
+    console.log(greeting);
+};
+
+sayHi();
+
+//console.log(firstName)
