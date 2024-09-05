e122a6e455e0b290e9e34fbbdd10fa92e2ab435a
will <william.layne.richardson@gmail.com>
2024-09-05 16:22:30 -0600

    game!

--- scripts/rps-game.js
+++ scripts/rps-game.js
@@ -5,26 +5,28 @@ let btnRock = document.getElementById("btn-rock");
 let btnPaper = document.getElementById("btn-paper");
 let btnScissors = document.getElementById("btn-scissors");
 
-function rockClicked(name){
-    console.log("rock was selected");
-}
+//function rockClicked(name){
+    //console.log("rock was selected");
+//}
 
-btnRock?.addEventListener("click",rockClicked);
+//btnRock?.addEventListener("click",rockClicked);
 
-function paperClicked(name){
-    console.log("paper was selected");
-}
+//function paperClicked(name){
+    //console.log("paper was selected");
+//}
 
-btnPaper?.addEventListener("click",paperClicked);
+//btnPaper?.addEventListener("click",paperClicked);
 
-function scissorsClicked(name){
-    console.log("scissor was selected");
-}
+//function scissorsClicked(name){
+   // console.log("scissor was selected");
+//}
 
-btnScissors?.addEventListener("click",scissorsClicked);
+//btnScissors?.addEventListener("click",scissorsClicked);
 
 function playerControlHandler(e){
-    console.log(e);
+    let weaponName = e.target.innerText;
+    console.log(weaponName, "was selected");
 }
 
-playerControls?.addEventListener("click",playerControlHandler)
+
+playerControls?.addEventListener("click",playerControlHandler);
