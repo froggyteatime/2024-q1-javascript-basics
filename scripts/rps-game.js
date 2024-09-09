627910d2e45fc1b09f89ab9c23d68e9b56faa12e
will <william.layne.richardson@gmail.com>
2024-09-09 16:23:15 -0600

    first game

--- scripts/rps-game.js
+++ scripts/rps-game.js
@@ -1,10 +1,11 @@
 //@ts-check
 
 let playerControls = document.getElementById("player-controls");
-let btnRock = document.getElementById("btn-rock");
-let btnPaper = document.getElementById("btn-paper");
-let btnScissors = document.getElementById("btn-scissors");
+//let btnRock = document.getElementById("btn-rock");
+//let btnPaper = document.getElementById("btn-paper");
+//let btnScissors = document.getElementById("btn-scissors");
 
+// let isGameOver = false
 //function rockClicked(name){
     //console.log("rock was selected");
 //}
@@ -43,9 +44,35 @@ function pickRandomWeapon(weapon){
     return weapons[randy];
 };
 
+function determineOutcome(playerWeapon,computerWeapon) {
+    if(playerWeapon.type === computerWeapon.type){
+        return "it's a tie! try again";
+    }
+
+    //isGameOver = true
+
+    if(playerWeapon.beats === computerWeapon.type){
+        return`player wins! ${playerWeapon.type} beats ${computerWeapon.type}`
+    }
+    return `computer wins! ${computerWeapon.type} beats ${playerWeapon.type}`
+}
+
 function playerControlHandler(e){
+    //if(isGameOver){
+     //   return;
+   // }
+
     let weaponName = e.target.innerText;
-    console.log(weaponName, "was selected");
+    let playerWeapon = weapons.find(w => w.type === weaponName);
+
+    if(!playerWeapon){
+        console.log("error! Player weapon undefined");
+        return;
+    }
+    let computerWeapon = pickRandomWeapon(weapons);
+
+    let result = determineOutcome(playerWeapon,computerWeapon);
+    console.log(result);
 }
 
 
