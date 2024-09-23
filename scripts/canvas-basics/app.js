d1b5d5566b2dac95724cdfb6c597ba8163429a8c
will <william.layne.richardson@gmail.com>
2024-09-23 16:17:25 -0600

    box

--- scripts/canvas-basics/app.js
+++ scripts/canvas-basics/app.js
@@ -1,14 +1,44 @@
 //@ts-check
 /** @type {HTMLCanvasElement} */
 //@ts-ignore canvas is an HTMLCanvasElement
-const canvas = document.getElementById("game-canvas")
+const canvas = document.getElementById("game-canvas");
 
 /** @type {CanvasRenderingContext2D} */
 //@ts-ignore canvas is an HTMLCanvasElement
 const ctx = canvas.getContext("2d");
 
-ctx.fillRect(0,0,50,50);
+ctx.fillStyle = "hsla(0,100%,50%,100%)";
+//"#00ff00"
+//"red"
+
+ctx.fillRect(0, 0, 50, 50);
 
 ctx.beginPath();
-ctx.arc(100,100,25,0,Math.PI * 2);
+ctx.arc(100, 100, 25, 0, Math.PI * 2);
 ctx.fill();
+
+let lastTime = 0;
+let hue = 0;
+
+let y1 = 0;
+let x1 = 0;
+let speedx = 1;
+let Speedy = 0.5;
+
+function drawLoop(timestamp) {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	let elapsedTime = timestamp - lastTime;
+	lastTime = timestamp;
+	//console.log(elapsedTime);
+	ctx.fillStyle = `hsla(${hue},100%,50%,100%)`;
+	ctx.fillRect(x1, y1, 100, 100);
+	//hue = hue + 1;
+	//hue += 1;
+	hue++;
+	x1 += speedx;
+	y1 += Speedy;
+
+	window.requestAnimationFrame(drawLoop);
+}
+
+window.requestAnimationFrame(drawLoop);
