24ee5f09108718ea526df29f4b436c2cd0f12639
will <william.layne.richardson@gmail.com>
2024-09-26 16:20:24 -0600

    yippe

--- scripts/canvas-basics/app.js
+++ scripts/canvas-basics/app.js
@@ -1,4 +1,5 @@
 //@ts-check
+import{SquareShape}from "./shapes/square.js";
 /** @type {HTMLCanvasElement} */
 //@ts-ignore canvas is an HTMLCanvasElement
 const canvas = document.getElementById("game-canvas");
@@ -7,58 +8,14 @@ const canvas = document.getElementById("game-canvas");
 //@ts-ignore canvas is an HTMLCanvasElement
 const ctx = canvas.getContext("2d");
 
-class SquareShape {
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
 
-		this.width = 50;
-		this.height = this.width;
-		this.hue = 0;
 
-		this.speedMult = 11;
-		this.speedX = Math.floor(Math.random() * 11) + 1;
-		this.speedY = Math.floor(Math.random() * 11) + 1;
-
-		this.dirx = 1;
-		this.diry = 1;
-	}
-
-	update() {
-		this.x += this.speedX * this.dirx;
-		this.y += this.speedY * this.diry;
-		this.hue++;
-
-		if (this.hue > 360) {
-			this.hue = 0;
-		}
-
-		if (this.x < 0) {
-			//0ff screen left so move right
-			this.dirx = 1;
-		} else if (this.x + this.width > canvas.width) {
-			this.dirx = -1;
-		}
-		if (this.y < 0) {
-			//0ff screen left so move right
-			this.diry = 1;
-		} else if (this.y + this.height > canvas.height) {
-			this.diry = -1;
-		}
-	}
-
-	draw() {
-		ctx.fillStyle = `hsla(${this.hue},100%,50%,100%)`;
-		ctx.fillRect(this.x, this.y, 100, 100);
-	}
-}
-
-let s1 = new SquareShape(0, 0);
+let s1 = new SquareShape(0, 0,ctx,canvas);
 
 let shapes =[];
 
 for (let i = 0; i < 100;i++) {
-	shapes.push(new SquareShape(0,0))
+	shapes.push(new SquareShape(0,0,ctx,canvas))
 }
 
 let lastTime = 0;
