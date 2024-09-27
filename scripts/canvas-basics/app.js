8630a8a05a80686eda91a0bd35acec4fa39012d9
will <william.layne.richardson@gmail.com>
2024-09-27 16:22:14 -0600

    random

--- scripts/canvas-basics/app.js
+++ scripts/canvas-basics/app.js
@@ -10,18 +10,18 @@ const ctx = canvas.getContext("2d");
 
 
 
-let s1 = new SquareShape(0, 0,ctx,canvas);
+let s1 = new SquareShape(Math.floor(Math.random()*1930)+ 1, Math.floor(Math.random()* 1930)+ 1,ctx,canvas);
 
 let shapes =[];
 
 for (let i = 0; i < 100;i++) {
-	shapes.push(new SquareShape(0,0,ctx,canvas))
+	shapes.push(new SquareShape(Math.floor(Math.random()*1930)+ 1, Math.floor(Math.random()* 1930)+ 1,ctx,canvas))
 }
 
 let lastTime = 0;
 
 function drawLoop(timestamp) {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.clearRect(0,0,canvas.width, canvas.height);
 	let elapsedTime = timestamp - lastTime;
 	lastTime = timestamp;
 
